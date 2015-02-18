2a35385c13212ac207ed03098b76283813fa8300
viitasusi <vllhtnn@gmail.com>
2015-02-18 14:17:42 +0200

    Viimeinen versio.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+/*Määritellään muuttujia ja hakemistopolku html ja javascript-tiedostoille*/
 var express = require('express');
 var app = express();
 var path = require("path");
 app.use(express.static(__dirname + '/public'));
 
+/*MySQL-yhteyttä varten tarvittavat muuttujat*/
 var mysql = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -11,10 +13,10 @@ var connection = mysql.createConnection({
   database : 'yritys'
 });
 
-
+/*Avataan yhteys kantaan*/
 connection.connect();
 
-
+/*Funktio hakee kaikki tiedot tyontekija-taulusta ja palauttaa taulukon*/
 app.get('/testi', function(req, res) {
 	var strQuery = 'select * from tyontekija';
 	var sendThis;
@@ -27,7 +29,20 @@ app.get('/testi', function(req, res) {
   });
 });
 
+/*Funktio hakee valitut tiedot toimipiste-taulusta ja palauttaa taulukon*/
+app.get('/testi2', function(req, res) {
+  var strQuery = 'SELECT nimi, katuosoite, email FROM toimipiste';
+  var sendThis;
 
+  connection.query( strQuery, function(err, rows, resp){
+      console.log( rows );
+      sendThis = rows //JSON.stringify(rows);
+      res.send(sendThis);
+    
+  });
+});
+
+/*Määritellään, palvelimen asetuksia*/
 var server = app.listen(3000, function () {
         var host = server.address().address
         var port = server.address().port
--- public/clientside.js
+++ public/clientside.js
@@ -1,10 +1,22 @@
+/*Funktio, joka kutsuu palvelinpään ohjelman metodia /testi.
+Kun funktio saa vastauksen, vastaus tulostetaan taulukkoon, joka lopuksi liitetään HTML-dokumenttiin.
+JQuerya käyttävä ratkaisu ei ole ihan oma luomukseni. Löysin ratkaisun StackOverflowsta ja muokkasin siitä 
+omaan tarkoitukseeni sopivan. Steal with pride!*/
 function funktio() {
 $.get('/testi', function(data){
 	var employees = data;
 	console.log(data);
+	
+	/*Luodaan pari muuttujaa taulukon ylimmän rivin tulostamista varten*/
+	var nimi = "<b>nimi</b>";
+	var ID = "<b>ID</b>";
 
+	/*Luodaan taulukko ja tulostetaan siihen työntekijän nimi ja ID*/
 	var $table = $( "<table></table>" );
-	var $line = $( "<tr></tr>" )
+	var $line = $( "<tr></tr>" );
+	$line.append( $( "<td></td>" ).html( nimi ) );
+	$line.append( $( "<td></td>" ).html( ID ) );
+	$table.append( $line );
 	
 
 	for ( var i = 0; i < employees.length; i++ ) {
@@ -15,10 +27,44 @@ $.get('/testi', function(data){
 	    $table.append( $line );
 	}
 
+/*Liitetään taulukko HTML-dokumenttiin*/
 $table.appendTo( document.body );
 
 
 });
 }
+
+/*Vastaavalla peraattteella toimiva funktio Yrityksen tomipisteiden tietojen tulostamiseen*/
+function funktio2() {
+$.get('/testi2', function(data){
+	var toimipisteet = data;
+	console.log(data);
+	var nimi = "<b>nimi</b>";
+	var katuosoite = "<b>katuosoite</b>";
+	var email = "<b>email</b>"
+
+	var $table = $( "<table></table>" );
+	var $line = $( "<tr></tr>" );
+	$line.append( $( "<td></td>" ).html( nimi ) );
+	$line.append( $( "<td></td>" ).html( katuosoite ) );
+	$line.append( $( "<td></td>" ).html( email ) );
+	$table.append( $line );
+	
+
+	for ( var i = 0; i < toimipisteet.length; i++ ) {
+	    var toim = toimipisteet[i];
+	    var $line = $( "<tr></tr>" );
+	    $line.append( $( "<td></td>" ).html( toim.nimi ) );
+	    $line.append( $( "<td></td>" ).html( toim.katuosoite ) );
+	    $line.append( $( "<td></td>" ).html( toim.email ) );
+	    $table.append( $line );
+	}
+
+$table.appendTo( document.body );
+
+
+});
+}
+
 
 
